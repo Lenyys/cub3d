33a7c648840543df6d920bc5ce853d8e14bed658
Lenyys <113938872+Lenyys@users.noreply.github.com>
2024-11-03 13:43:10 +0100

    Update init.c

--- init.c
+++ init.c
@@ -9,18 +9,3 @@
 /*   Updated: 2024/11/02 11:14:06 by rnovotny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "cub3d.h"
-
-int	init_map(t_game *game, char *arg)
-{
-	int fd;
-	
-	fd = open(arg, O_RDONLY);
-	if (fd < 0)
-	{
-		ft_printf("Error\nCould not open file\n");
-		return(1);
-	}
-	
-}
